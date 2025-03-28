3c3826128c2c1e7a01f01074a44496cefcedc3ce
Siddharth Mishra <siddm2410@gmail.com>
2025-03-28 18:26:15 +0530

    Init

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import React from 'react';
 import styles from '../styles/Contact.module.css';
+import styled from 'styled-components';
+
 
 const ContactMe: React.FC = () => {
 
@@ -30,36 +34,107 @@ const ContactMe: React.FC = () => {
           </p>
         </div>
       </div>
+      
 
-      {/* Social Media Links */}
-      <div className={styles.socialLinks}>
-        <a 
-          href="https://www.linkedin.com/in/siddharth-mishra-882035352/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.socialLink}
-        >
-          <img title='LinkedIn Profile' src="/linkedinlogo.png" alt="LinkedIn" className={styles.icon} />
+      <StyledWrapper>
+      <div className="card">
+
+        <a className="socialContainer containerOne" href="#">
+          <img title='Github Profile' className="socialimg githubimg" src="/GitHub.png" alt="logo" />
         </a>
-        <a
-          href="https://github.com/siddmishra7"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.socialLink}
-        >
-          <img title='Github Profile' src="/githublogo.png" alt="GitHub" className={styles.icon} />
+        <a className="socialContainer containerThree" href="#">
+        <img title='Linkedin Profile' className="socialimg linkedinimg" src="/linkedin2.jpg" alt="logo" />
         </a>
-        <a
-          href="https://x.com/sidd12112"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.socialLink}
-        >
-          <img title='X Profile' src="/xlogobg.png" alt="Twitter" className={styles.xlogo} />
+        <a className="socialContainer containerFour" href="#">
+        <img title='X Profile' className="socialimgx xlogoimg" src="/xlogo1.png" alt="logo" />
         </a>
       </div>
+    </StyledWrapper>
     </section>
   );
 };
+
+const StyledWrapper = styled.div`
+  .card {
+    width: fit-content;
+    height: fit-content;
+    margin: auto;
+    background-color: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px 10px;
+    gap: 20px;
+  }
+
+  /* for all social containers*/
+  .socialContainer {
+    width: 52px;
+    height: 52px;
+    border-radius: 50%;
+    background-color: #525252;
+    border: 2px solid white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    transition-duration: 1s;
+  }
+
+  .socialContainer:hover{
+     transform: translateY(-5px);
+  }
+
+  /* instagram*/
+  .containerOne:hover {
+    background-color: white;
+    transition-duration: 0.3s;
+  }
+  /* twitter*/
+  .containerTwo:hover {
+    background-color: #00acee;
+    transition-duration: 0.3s;
+  }
+  /* linkdin*/
+  .containerThree:hover {
+    background-color: #0072b1;
+    transition-duration: 0.3s;
+  }
+  /* Whatsapp*/
+  .containerFour:hover {
+    background-color: white;
+    transition-duration: 0.3s;
+  }
+
+  .socialContainer:active {
+    transform: scale(0.9);
+    transition-duration: 0.3s;
+  }
+
+  .socialimg {
+    width: 40px;
+  }
+
+  .socialimgx{
+    width: 30px
+  }
+
+
+  .socialContainer:hover .socialSvg {
+    animation: slide-in-top 0.3s both;
+  }
+
+  @keyframes slide-in-top {
+    0% {
+      transform: translateY(-50px);
+      opacity: 0;
+    }
+
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }`;
+
 
 export default ContactMe;
